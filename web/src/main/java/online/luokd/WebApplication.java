d628866040ca14d0ba7c862a03833f0e9fc87393
LuoKa <luo_kd@gzhc365.com>
2020-03-24 10:45:51 +0800

    WEB

--- web/src/main/java/online/luokd/WebApplication.java
+++ web/src/main/java/online/luokd/WebApplication.java
@@ -1,19 +1,17 @@
 package online.luokd;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("online.luokd.mapper")
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class,args);
-    }
-
-    @RequestMapping(value = "hi")
-    public String hello(){
-        return "hello,earth";
     }
 }
